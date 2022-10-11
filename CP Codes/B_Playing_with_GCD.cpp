3d83f6ba98ca6f53f59892c20a2182abb01d2719
halpaniraj.hr4823@gmail.com <halpaniraj.hr4823@gmail.com>
2022-10-11 20:45:19 +0530

    Round 825

---
+++ CP Codes/B_Playing_with_GCD.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vpll vector<pair<ll, ll>>
+#define vvll vector<vector<ll>>
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define mll map<ll, ll>
+#define fauto(i, a) for (auto &i : a)
+#define f(i, n) for (int i = 0; i < (n); i++)
+#define ff(i, a, b) for (int i = (a); i < (b); i++)
+#define fr(i, n) for (int i = (n); i >= 0; i--)
+#define ffr(i, a, b) for (int i = (a); i >= (b); i--)
+#define db1(x) cout << #x << " = " << (x) << "\n";
+#define db2(x, y) cout << #x << " = " << (x) << " " << #y << " = " << (y) << "\n"
+#define SIZE 1000001
+#define MOD 1000000007LL
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vll v(n);
+        bool flag = true;
+
+        f(i, n) cin >> v[i];
+        f(i, n - 2)
+        {
+            if (__gcd(v[i], v[i + 2]) > __gcd(v[i], v[i + 1]))
+            {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
---
+++ CP Codes/C_1_Good_Subarrays_Easy_Version.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vpll vector<pair<ll, ll>>
+#define vvll vector<vector<ll>>
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define mll map<ll, ll>
+#define fauto(i, a) for (auto &i : a)
+#define f(i, n) for (int i = 0; i < (n); i++)
+#define ff(i, a, b) for (int i = (a); i < (b); i++)
+#define fr(i, n) for (int i = (n); i >= 0; i--)
+#define ffr(i, a, b) for (int i = (a); i >= (b); i--)
+#define db1(x) cout << #x << " = " << (x) << "\n";
+#define db2(x, y) cout << #x << " = " << (x) << " " << #y << " = " << (y) << "\n"
+#define SIZE 1000001
+#define MOD 1000000007LL
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vll v(n);
+        f(i, n) cin >> v[i];
+
+        ll index = 0, ans = 0;
+        ll count = 1;
+
+        f(i, n)
+        {
+            while (index < n && v[i] < count)
+            {
+                count--;
+                index++;
+            }
+            ans += (i - index + 1);
+            count++;
+        }
+        cout << ans << "\n";
+    }
+    return 0;
+}
