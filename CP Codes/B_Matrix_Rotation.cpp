94d5754c4b8952d9c7f479ee80f214ea35b76ba7
halpaniraj.hr4823@gmail.com <halpaniraj.hr4823@gmail.com>
2022-12-25 22:05:53 +0530

    Practice

---
+++ CP Codes/B_Matrix_Rotation.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vpll vector<pair<ll, ll>>
+#define vvll vector<vector<ll>>
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define mll map<ll, ll>
+#define fauto(i, a) for (auto &i : a)
+#define f(i, n) for (int i = 0; i < (n); i++)
+#define ff(i, a, b) for (int i = (a); i < (b); i++)
+#define fr(i, n) for (int i = (n); i >= 0; i--)
+#define ffr(i, a, b) for (int i = (a); i >= (b); i--)
+#define db1(x) cout << #x << " = " << (x) << "\n";
+#define db2(x, y) cout << #x << " = " << (x) << " " << #y << " = " << (y) << "\n"
+#define SIZE 1000001
+#define MOD 1000000007LL
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a[2][2] = {0};
+        pair<int, int> maxi, mini;
+        maxi = {0, 0};
+        mini = {0, 0};
+        f(i, 2)
+        {
+            f(j, 2)
+            {
+                cin >> a[i][j];
+                if (a[i][j] > a[maxi.first][maxi.second])
+                    maxi = {i, j};
+                if (a[i][j] < a[mini.first][mini.second])
+                    mini = {i, j};
+            }
+        }
+        // db2(maxi.first, maxi.second);
+        // db2(mini.first, mini.second);
+        if (maxi.first != mini.first && maxi.second != mini.second)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
